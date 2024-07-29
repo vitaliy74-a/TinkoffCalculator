5d5b40abf6fe7ef2e2f6f6d21a5ea57629928221
vitaliy74-a <vitaliyp1974@gmail.com>
2024-07-29 21:50:37 +0300

    Initial Commit

---
+++ TinkoffCalculator/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  TinkoffCalculator
+//
+//  Created by Виталий Пасынок on 29.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+    @IBAction func rrr(_ sender: UIButton) {
+        guard let buttonText = sender.currentTitle else { return }
+        print(buttonText)
+    }
+    
+    
+    
+      
+    
+    
+    
+
+    
+    
+    
+  
+   
+   
+    
+    
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    
+    }
+
+
+   
+}
+
